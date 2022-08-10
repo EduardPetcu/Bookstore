714e0276a8cff3658be8baa588cb31bd85df82e4
EduardPetcu <eduardpetcu4@gmail.com>
2022-08-10 18:06:31 +0300

    Added homework
    Signed-off-by: EduardPetcu <eduardpetcu4@gmail.com>

---
+++ sqlscripts/insertAdmin.sql
@@ -0,0 +1,2 @@
+INSERT INTO user (email,name,password,role,username) 
+VALUES ("admin@gmail.com","Andrei Root","root123","admin","root_user");
---
+++ sqlscripts/insertAuthors.sql
@@ -0,0 +1,5 @@
+INSERT INTO author VALUES (1,1850,"Mihai Eminescu");
+INSERT INTO author (birth_year,name) VALUES (1965 ,"J. K. Rowling");
+INSERT INTO author (birth_year,name) VALUES (1890,"Agatha Christie");
+INSERT INTO author (birth_year,name) VALUES (1903,"George Orwell");
+INSERT INTO author (birth_year,name) VALUES (1852,"Ion Luca Caragiale");
